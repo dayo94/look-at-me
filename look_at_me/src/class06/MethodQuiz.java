96fc5319625f14746f951b19f90398c070b0de58
dayo94 <joungaejs4@naver.com>
2021-07-08 00:07:38 +0900

    method

---
+++ look_at_me/src/class06/MethodQuiz.java
@@ -0,0 +1,47 @@
+package class06;
+
+public class MethodQuiz {
+
+	
+
+
+	public void print100() {
+		for(int i=1; i<101; i++) {
+			System.out.println(i);
+		}
+	}
+	
+	
+	
+	
+
+	
+	public int printHello( int num ) {
+		for(int i=0; i<num; i++) {
+			System.out.println("Hello");
+		}
+		return num;
+	}
+	
+
+	
+	public String printText(int cnt, String text) {
+		
+		for(int i=0; i<cnt; i++) {
+			System.out.println(text);
+		}
+		return text;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}//class
