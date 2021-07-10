d575489e907a87cb8181a948132733f09c50e735
dayo94 <joungaejs4@naver.com>
2021-07-10 21:47:40 +0900

    uml car

---
+++ look_at_me/src/class06/uml/CarEx.java
@@ -0,0 +1,31 @@
+package class06.uml;
+
+public class CarEx {
+
+	public static void main(String[] args) {
+
+
+		
+
+		Car car = new Car("K5", "Black");
+		car.display();
+		
+		System.out.println("----------");
+		
+		car.setModel("¸ð´×");
+		car.setColor("White");
+		
+		System.out.println( car.getModel() );
+		System.out.println( car.getColor() );
+		
+		System.out.println("----------");
+		car.display();
+		
+
+		
+		
+		
+	}//main
+	
+	
+}//class
