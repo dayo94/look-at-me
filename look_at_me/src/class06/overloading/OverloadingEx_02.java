1f1866be5e6a330da7529f2a90c05e31e27ba3fe
dayo94 <joungaejs4@naver.com>
2021-07-11 22:56:16 +0900

    overlodaing

---
+++ look_at_me/src/class06/overloading/OverloadingEx_02.java
@@ -0,0 +1,25 @@
+package class06.overloading;
+
+public class OverloadingEx_02 {
+
+	public static void main(String[] args) {
+
+		Overloading ol = new Overloading();
+
+		System.out.println("--- display() ---");
+		ol.display();
+
+
+		System.out.println("--- display(11,22) ---");
+		ol.display(11, 22);
+
+		System.out.println("--- display() ---");
+		ol.display();
+
+
+
+
+
+	}//main
+}
+//class
