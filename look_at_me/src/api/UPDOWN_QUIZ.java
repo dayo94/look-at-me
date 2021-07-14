424b00484a831bb65c941308e2b2a2bd1b88d2cf
dayo94 <joungaejs4@naver.com>
2021-07-14 23:51:30 +0900

    updownQuiz

---
+++ look_at_me/src/api/UPDOWN_QUIZ.java
@@ -0,0 +1,48 @@
+package api;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class UPDOWN_QUIZ {
+
+	public static void main(String[] args) {
+
+
+		Random ran = new Random();
+
+		int com = (ran.nextInt(50))+1;
+
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("숫자를 입력하세요");
+		System.out.print(">>");
+		int user = sc.nextInt();
+
+		for(int i=0; i<7; i++) {
+			if(user<com) {
+				System.out.println("up");
+				System.out.println("숫자를 입력하세요");
+				System.out.print(">>");
+				user = sc.nextInt();
+
+			} else if (user>com) {
+				System.out.println("down");
+				System.out.println("숫자를 입력하세요");
+				System.out.print(">>");
+				user = sc.nextInt();
+			} else if (user == com) {
+				System.out.println("user 승리");
+				return;
+			} else if (i==6) {
+				
+				System.out.println("com 승리");			
+			}
+			
+		}
+
+
+
+	}//main
+
+}//class
